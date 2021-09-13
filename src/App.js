68e18d9617d6cfcc7e23ea56a29b406d7f2aa3f8
narahda <limnarahda@gmail.com>
2021-09-13 17:23:43 +0800

    first steps!
    added image descriptions for card,  changed App page now, should link to linkedin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <><div className= "nara">
+        <Navbar dark color = "dark">
+          <div className = "container">
+            <NavbarBrand href = "https://www.linkedin.com/in/narahda-lim-3b22891a4/"> About Me hehe
+            </NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+
+      <div className = "voldetort">
+        <Navbar dark color = "light">
+          <div className = "container">
+            <NavbarBrand href = "https://www.reddit.com/r/WhitePeopleTwitter/comments/7lkizz/voldetort/">
+            </NavbarBrand>
+          </div>
+        </Navbar>
+      </div> </>
+    );
+  }
 }
 
 export default App;
---
+++ src/info/info.js
@@ -0,0 +1,14 @@
+export const information =
+    [
+        {
+            id: 0,
+            name: 'orange',
+            image: 'pictures/orange.jpg',
+            description: 'orange is a fruit, orange is a colour. did orange or orange come first? heres a remnant of the 2000s anyways'
+        } {
+            id: 1,
+            name: 'voldetort',
+            image: 'pictures/voldetort.jfif',
+            description: 'voldemort is a turtle. why? they both lack a human-ish nose. they are both bald. they both have reptile companions. therefore voldemort is an honorary turtle'
+        }
+    ]
