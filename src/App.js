4ed2e396b39a18eb3a1d845c845a03c8ce054159
narahda <limnarahda@gmail.com>
2021-09-14 21:04:25 +0800

    Update App.js
    I FIGURED OUT HOW TO IMPORT THE COMPONENT

--- src/App.js
+++ src/App.js
@@ -19,12 +19,8 @@ class App extends Component {
   
   render() {
     return (
-      <div className= "boutme">
-        <Navbar dark color = "dark">
-          <div className = "container">
-            <NavbarBrand href = "https://www.linkedin.com/in/narahda-lim-3b22891a4/"> About Me hehe</NavbarBrand>
-          </div>
-        </Navbar>
+      <div>
+        <Navi></Navi>
       </div>
     );
   }
